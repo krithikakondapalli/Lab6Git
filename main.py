d15bca67fcbca3b63f14eaa5bfe528ee6b86d8ad
kayleezamora <kayzlee14x@gmail.com>
2023-10-25 18:49:15 -0400

    new changes

--- main.py
+++ main.py
@@ -35,6 +35,21 @@ def encode(password):
 
     return encoded_pass
 
+def decode(decode_str):
+    password_str = ''
+    for i in decode_str:
+        if i == '0':
+            str(password_str)
+            password_str += '7'
+        elif i == '1':
+            str(password_str)
+            password_str += '8'
+        elif i == '2':
+            str(password_str)
+            password_str += '9'
+        else:
+            password_str += str(int(i) - 3)
+    return password_str
 
 
 if __name__ == "__main__":
